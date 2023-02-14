27b420e7f1390eb92d9aef4a5861644d530301ab
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-02-14 13:52:31 -0300

    added video again

--- src/post.js
+++ src/post.js
@@ -1,147 +1,147 @@
 export const posts = [
-//     {
-//     id:1,
-//     postProfileName: "Eduardo S. Santos",
-//     postProfileImageUrl: "assets/history-1/eu.jpg",
-//     postContent: { isVideo: true, url:"assets/video"},
-//     postLikes: 10,
-//     postComments: [{
-//         id:1,
-//         commentImgUrl: "assets/fotos-T10/felipe.jpeg",
-//         commentName: "Felipe",
-//         commentTimeAgo: "5d",
-//         commentText: "TA MALUCOOO que lindoo",
-//         commentLikes: 3
-//     },
-//     {
-//         id:2,
-//         commentImgUrl: "assets/fotos-T10/felipe.jpeg",
-//         commentName: "Felipe",
-//         commentTimeAgo: "5d",
-//         commentText: "tem que entregar só semana que vem né !?",
-//         commentLikes: 8
-//     },
-//     {
-//         id:3,
-//         commentImgUrl: "assets/fotos-T10/filipe.jpeg",
-//         commentName: "Filipe",
-//         commentTimeAgo: "5d",
-//         commentText: "quinta",
-//         commentLikes: 2
-//     },
-//     {
-//         id:4,
-//         commentImgUrl: "assets/fotos-T10/felipe.jpeg",
-//         commentName: "Felipe",
-//         commentTimeAgo: "5d",
-//         commentText: "ficou mara. perfeitinho",
-//         commentLikes: 9
-//     },
-//     {
-//         id:5,
-//         commentImgUrl: "assets/fotos-T10/filipe.jpeg",
-//         commentName: "Filipe",
-//         commentTimeAgo: "5d",
-//         commentText: "era p essa pergunta kkk",
-//         commentLikes: 1
-//     },
-//     {
-//         id:6,
-//         commentImgUrl: "assets/fotos-T10/filipe.jpeg",
-//         commentName: "Filipe",
-//         commentTimeAgo: "5d",
-//         commentText: "Parabéns Eduardo ficou demais hein!!!",
-//         commentLikes: 4
-//     },
-//     {
-//         id:7,
-//         commentImgUrl: "assets/fotos-T10/no-img.jpg",
-//         commentName: "Felipe Pires Lasbik",
-//         commentTimeAgo: "5d",
-//         commentText: "👏👏👏",
-//         commentLikes: 0
-//     },
-//     {
-//         id:8,
-//         commentImgUrl: "assets/fotos-T10/matheus-perion.jpeg",
-//         commentName: "Matheus Perion",
-//         commentTimeAgo: "5d",
-//         commentText: "ta ai o homem que vai me ajudar",
-//         commentLikes: 3
-//     },
-//     {
-//         id:9,
-//         commentImgUrl: "assets/fotos-T10/matheus-wagner.jpeg",
-//         commentName: "mATHEUS WAGNER",
-//         commentTimeAgo: "5d",
-//         commentText: "Ficou com mais cara de Instagram do que o próprio Instagram",
-//         commentLikes: 3
-//     },
-//     {
-//         id:10,
-//         commentImgUrl: "assets/fotos-T10/no-img.jpg",
-//         commentName: "Felipe Pires Lasbik",
-//         commentTimeAgo: "5d",
-//         commentText: "REALMENTE",
-//         commentLikes: 9
-//     },
-//     {
-//         id:11,
-//         commentImgUrl: "assets/fotos-T10/felipe.jpeg",
-//         commentName: "Felipe",
-//         commentTimeAgo: "5d",
-//         commentText: "Papai vem almoçar”””” aaaii meu deus meu coração",
-//         commentLikes: 15
-//     },
-//     {
-//         id:12,
-//         commentImgUrl: "assets/fotos-T10/quell.jpeg",
-//         commentName: "Raquell Victória",
-//         commentTimeAgo: "5d",
-//         commentText: "vem almoçar🥺🥺 q fofinho",
-//         commentLikes: 30
-//     },
-//     {
-//         id:13,
-//         commentImgUrl: "assets/fotos-T10/tamara.jpeg",
-//         commentName: "Tamara",
-//         commentTimeAgo: "5d",
-//         commentText: '"O papai, vem almoçar"',
-//         commentLikes: 31
-//     },
-//     {
-//         id:14,
-//         commentImgUrl: "assets/fotos-T10/tamara.jpeg",
-//         commentName: "Tamara",
-//         commentTimeAgo: "5d",
-//         commentText: "Anwt",
-//         commentLikes: 32
-//     },
-//     {
-//         id:15,
-//         commentImgUrl: "assets/fotos-T10/tamara.jpeg",
-//         commentName: "Tamara",
-//         commentTimeAgo: "5d",
-//         commentText: "Desde cedo aprendendo a tirar o pai dev do computador se não ele não sai pra comer kkkkkkkkkkkkkkk",
-//         commentLikes: 32
-//     },
-//     {
-//         id:16,
-//         commentImgUrl: "assets/fotos-T10/no-img.jpg",
-//         commentName: "Vinicius Vieira",
-//         commentTimeAgo: "5d",
-//         commentText: "Lindo demais !!!!",
-//         commentLikes: 23
-//     },
-//     {
-//         id:17,
-//         commentImgUrl: "assets/fotos-T10/matheus-barcelos.jpeg",
-//         commentName: "Matheus Barcelos",
-//         commentTimeAgo: "5d",
-//         commentText: 'Esse "Oi Mateus" me fez fechar todos os programas devoice q estavam aberto kkk',
-//         commentLikes: 53
-//     }]
-// }, 
+    {
+    id:1,
+    postProfileName: "Eduardo S. Santos",
+    postProfileImageUrl: "assets/history-1/eu.jpg",
+    postContent: { isVideo: true, url:"assets/video"},
+    postLikes: 10,
+    postComments: [{
+        id:1,
+        commentImgUrl: "assets/fotos-T10/felipe.jpeg",
+        commentName: "Felipe",
+        commentTimeAgo: "5d",
+        commentText: "TA MALUCOOO que lindoo",
+        commentLikes: 3
+    },
+    {
+        id:2,
+        commentImgUrl: "assets/fotos-T10/felipe.jpeg",
+        commentName: "Felipe",
+        commentTimeAgo: "5d",
+        commentText: "tem que entregar só semana que vem né !?",
+        commentLikes: 8
+    },
+    {
+        id:3,
+        commentImgUrl: "assets/fotos-T10/filipe.jpeg",
+        commentName: "Filipe",
+        commentTimeAgo: "5d",
+        commentText: "quinta",
+        commentLikes: 2
+    },
+    {
+        id:4,
+        commentImgUrl: "assets/fotos-T10/felipe.jpeg",
+        commentName: "Felipe",
+        commentTimeAgo: "5d",
+        commentText: "ficou mara. perfeitinho",
+        commentLikes: 9
+    },
+    {
+        id:5,
+        commentImgUrl: "assets/fotos-T10/filipe.jpeg",
+        commentName: "Filipe",
+        commentTimeAgo: "5d",
+        commentText: "era p essa pergunta kkk",
+        commentLikes: 1
+    },
+    {
+        id:6,
+        commentImgUrl: "assets/fotos-T10/filipe.jpeg",
+        commentName: "Filipe",
+        commentTimeAgo: "5d",
+        commentText: "Parabéns Eduardo ficou demais hein!!!",
+        commentLikes: 4
+    },
+    {
+        id:7,
+        commentImgUrl: "assets/fotos-T10/no-img.jpg",
+        commentName: "Felipe Pires Lasbik",
+        commentTimeAgo: "5d",
+        commentText: "👏👏👏",
+        commentLikes: 0
+    },
+    {
+        id:8,
+        commentImgUrl: "assets/fotos-T10/matheus-perion.jpeg",
+        commentName: "Matheus Perion",
+        commentTimeAgo: "5d",
+        commentText: "ta ai o homem que vai me ajudar",
+        commentLikes: 3
+    },
+    {
+        id:9,
+        commentImgUrl: "assets/fotos-T10/matheus-wagner.jpeg",
+        commentName: "mATHEUS WAGNER",
+        commentTimeAgo: "5d",
+        commentText: "Ficou com mais cara de Instagram do que o próprio Instagram",
+        commentLikes: 3
+    },
+    {
+        id:10,
+        commentImgUrl: "assets/fotos-T10/no-img.jpg",
+        commentName: "Felipe Pires Lasbik",
+        commentTimeAgo: "5d",
+        commentText: "REALMENTE",
+        commentLikes: 9
+    },
+    {
+        id:11,
+        commentImgUrl: "assets/fotos-T10/felipe.jpeg",
+        commentName: "Felipe",
+        commentTimeAgo: "5d",
+        commentText: "Papai vem almoçar”””” aaaii meu deus meu coração",
+        commentLikes: 15
+    },
+    {
+        id:12,
+        commentImgUrl: "assets/fotos-T10/quell.jpeg",
+        commentName: "Raquell Victória",
+        commentTimeAgo: "5d",
+        commentText: "vem almoçar🥺🥺 q fofinho",
+        commentLikes: 30
+    },
+    {
+        id:13,
+        commentImgUrl: "assets/fotos-T10/tamara.jpeg",
+        commentName: "Tamara",
+        commentTimeAgo: "5d",
+        commentText: '"O papai, vem almoçar"',
+        commentLikes: 31
+    },
+    {
+        id:14,
+        commentImgUrl: "assets/fotos-T10/tamara.jpeg",
+        commentName: "Tamara",
+        commentTimeAgo: "5d",
+        commentText: "Anwt",
+        commentLikes: 32
+    },
+    {
+        id:15,
+        commentImgUrl: "assets/fotos-T10/tamara.jpeg",
+        commentName: "Tamara",
+        commentTimeAgo: "5d",
+        commentText: "Desde cedo aprendendo a tirar o pai dev do computador se não ele não sai pra comer kkkkkkkkkkkkkkk",
+        commentLikes: 32
+    },
+    {
+        id:16,
+        commentImgUrl: "assets/fotos-T10/no-img.jpg",
+        commentName: "Vinicius Vieira",
+        commentTimeAgo: "5d",
+        commentText: "Lindo demais !!!!",
+        commentLikes: 23
+    },
+    {
+        id:17,
+        commentImgUrl: "assets/fotos-T10/matheus-barcelos.jpeg",
+        commentName: "Matheus Barcelos",
+        commentTimeAgo: "5d",
+        commentText: 'Esse "Oi Mateus" me fez fechar todos os programas devoice q estavam aberto kkk',
+        commentLikes: 53
+    }]
+}, 
 {
     id:2,
     postProfileName: "SunsetGuy",
